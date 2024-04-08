4890e555f219fc0c7def8bd73c22569fe7519bed
antoniobiasotti <abamorim36@gmail.com>
2024-04-08 02:25:43 -0300

    User profile page

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -1,10 +1,14 @@
 import { ReactNode } from 'react'
+import { Sidebar } from '../../components/Sidebar/Sidebar.jsx'
+import { Form } from '../../components/Form/Form.jsx';
 
 export default function UserLayout({ children} : {children: ReactNode }){
     return (
         <div>
             <div>{children}</div>
             <p>Perfil do Usuário</p>
+            <Sidebar/>
+            <Form />
         </div>
     )
 }
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,19 +1,26 @@
+import { PencilLine } from 'phosphor-icons/dist/ssr'
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
     return (
         <aside className={styles.sidebar}>
-            <div className={styles.cover}></div>
-            <div className={"bi bi-person-circle"}>
-                <strong>Username</strong>
-                <span></span>
-            </div>
+               <img 
+                    className={styles.cover}
+                    src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50" 
+               />
 
-            <footer>
-                <a href="#">
-                    Editar seu perfil
-                </a>
-            </footer>
-        </aside>
+               <div className={styles.profile}>
+
+                    <strong>Antônio Biasotti</strong>
+                    <span>Web Developer</span>
+               </div>
+
+               <footer>
+                    <a href="#">
+                         <PencilLine size={20}/>
+                         Editar seu perfil
+                    </a>
+               </footer>
+          </aside>
     )
 }
