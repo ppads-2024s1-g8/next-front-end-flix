036e1d00bfa796b596cf14c1a03acfb4616ac8db
antoniobiasotti <abamorim36@gmail.com>
2024-04-14 15:15:56 -0300

    add: Dashboard page with users list

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,10 +1,13 @@
 import { ReactNode } from 'react'
+import { Header } from '../../components/Header/Header'
+import { UsersList } from 'src/components/DashBoard/users-list'
 
 export default function AdminLayout({ children} : {children: ReactNode }){
     return (
         <div>
+            <Header/>
             <div>{children}</div>
-            <p>Painel de Administração</p>
+            <UsersList/>
         </div>
     )
 }
